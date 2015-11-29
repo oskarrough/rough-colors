d7da5137be2ff290ecbe5b457b4ef7082009c631
Oskar Rough <oskar@rough.dk>
2015-11-29 23:48:04 +0100

    Add CORS, change API to use ?url= queries.

--- index.js
+++ index.js
@@ -6,38 +6,68 @@ const app = express();
 
 const extractColors = require('./src/extract-colors');
 const checkValidUrl = require('./src/check-valid-url');
+const checkUrlExists = require('./src/check-url-exists');
 const serializer = require('./src/serializer');
 
-app.get('/', function (request, response) {
-	response.send('Rough colors.');
+app.use(function(request, response, next) {
+	response.header('Access-Control-Allow-Origin', '*');
+	response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
 });
 
-app.get('/v1/images/*', function (request, response) {
-	let url = request.params[0];
+app.get('/', function (request, response) {
+	let url = request.query.url;
+
+	// Break if we don't have a URL.
+	if (!url) {
+		console.log('no url');
+		response.status(500).send('Specify a ?url=http://… query, please.');
+		return;
+	}
 
 	// Break if we don't have a valid URL.
 	if (!checkValidUrl(url)) {
+		console.log('not valid url');
 		response.send('not a valid URL');
 		return;
 	}
 
-	// ?mapped=true return 3x3 mapped colors
-	if (request.query.mapped) {
-		extractColors.mapped(url, function (colors) {
+	// Break if the URL doesn't exist (we request and test)
+	checkUrlExists(url, function (exists) {
+		if (!exists) {
+			console.log('does not exist');
+			response.status(500).send('Could not fetch image. Is the url valid?');
+			return;
+		}
+
+		url = url.replace('https://', 'http://');
+
+		// ?mapped=true return 3x3 mapped colors
+		if (request.query.mapped) {
+			extractColors.mapped(url, function (colors) {
+				response.jsonp(serializer.format(colors));
+			});
+			return;
+		}
+
+		// By default returns all colors sorted by frequency
+		extractColors.all(url, function (colors) {
 			response.jsonp(serializer.format(colors));
 		});
-		return;
-	}
-
-	// By default returns all colors sorted by frequency
-	extractColors.all(url, function (colors) {
-		response.jsonp(serializer.format(colors));
 	});
 });
+
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('error', { error: err });
+}
 
 const server = app.listen(3000, function () {
   const host = server.address().address;
   const port = server.address().port;
 
-  console.log(`Example app listening at http://${host}:${port}`);
+  console.log(`Rough colors listening at http://${host}:${port}`);
 });
